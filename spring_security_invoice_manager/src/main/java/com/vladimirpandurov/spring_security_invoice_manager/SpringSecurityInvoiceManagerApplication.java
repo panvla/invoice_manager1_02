b0cd0db1b7bfff053d4d309c49813e36a4af168b
panvla <pandvlado@gmail.com>
2024-02-02 09:44:18 +0100

    access denied handler and authentication entry point and security config added.

--- spring_security_invoice_manager/src/main/java/com/vladimirpandurov/spring_security_invoice_manager/SpringSecurityInvoiceManagerApplication.java
+++ spring_security_invoice_manager/src/main/java/com/vladimirpandurov/spring_security_invoice_manager/SpringSecurityInvoiceManagerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication//(exclude = SecurityAutoConfiguration.class)
 @CrossOrigin("*")
 public class SpringSecurityInvoiceManagerApplication {
 
---
+++ spring_security_invoice_manager/src/main/java/com/vladimirpandurov/spring_security_invoice_manager/configuration/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.vladimirpandurov.spring_security_invoice_manager.configuration;
+
+import com.vladimirpandurov.spring_security_invoice_manager.handler.CustomAccessDeniedHandler;
+import com.vladimirpandurov.spring_security_invoice_manager.handler.CustomAuthenticationEntryPoint;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.parameters.P;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final UserDetailsService userDetailsService;
+    private final CustomAccessDeniedHandler customAccessDeniedHandler;
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+    private final BCryptPasswordEncoder encoder;
+    private static final String[] PUBLIC_URLS = {"/user/login/**", "/user/register/**"};
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeHttpRequests().requestMatchers(PUBLIC_URLS).permitAll();
+        http.authorizeHttpRequests().requestMatchers(HttpMethod.DELETE, "/user/delete/**").hasAnyAuthority("DELETE:USER");
+        http.authorizeHttpRequests().requestMatchers(HttpMethod.DELETE, "/customer/delete/**").hasAnyAuthority("DELETE:CUSTOMER");
+
+        http.exceptionHandling().accessDeniedHandler(customAccessDeniedHandler).authenticationEntryPoint(customAuthenticationEntryPoint);
+        http.authorizeHttpRequests().anyRequest().authenticated();
+        return http.build();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(encoder);
+        return new ProviderManager(authenticationProvider);
+    }
+}
